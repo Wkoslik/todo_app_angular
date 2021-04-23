69fe9ce61043fa5a380c2461815aabb4ba122f65
wkoslik <wkoslik@gmail.com>
2021-04-23 15:44:27 -0400

    input component functionality, sort of

--- src/app/input-item/input-item.component.ts
+++ src/app/input-item/input-item.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-item.component.css']
 })
 export class InputItemComponent implements OnInit {
+  title: string = '';
+  description: string = '';
+  dueDate: string;
+  tags: string ='';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  createNewTodo(newTitle: string, newDesc: string, newDueDate: string, newTags: string): void {
+    event.preventDefault();
+    this.title = newTitle;
+    this.description = newDesc;
+    this.dueDate = newDueDate;
+    this.tags = newTags;
+    console.log(this.title, this.description, this.dueDate, this.tags)
+  }
 }
